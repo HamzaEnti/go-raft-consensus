9a6ac4703035c34a65a247b4ba7a0c86f1a492a2
HamzaEnti <hamza.elhaddad@enti.cat>
2025-12-27 17:49:18 +0100

    Add basic Raft types and state definitions
    Define basic Raft message types and node states.

--- types.go
+++ types.go
@@ -1,1 +1,44 @@
+package main
 
+// ----------------------------
+// Basic Raft Types
+// ----------------------------
+
+type MsgType int
+
+const (
+	MsgRequestVote MsgType = iota
+	MsgRequestVoteResp
+	MsgAppendEntries     // Heartbeat
+	MsgAppendEntriesResp // Not heavily used in this simple version
+)
+
+type Message struct {
+	From        int
+	To          int
+	Term        int
+	Type        MsgType
+	VoteGranted bool // Only used in MsgRequestVoteResp
+}
+
+// Node State
+type State int
+
+const (
+	StateFollower State = iota
+	StateCandidate
+	StateLeader
+)
+
+func (s State) String() string {
+	switch s {
+	case StateFollower:
+		return "Follower"
+	case StateCandidate:
+		return "Candidate"
+	case StateLeader:
+		return "Leader"
+	default:
+		return "Unknown"
+	}
+}
